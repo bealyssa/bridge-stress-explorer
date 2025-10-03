3c1d13aefe672d990607e46339f640c7fb76ab3e
Bea Alyssa Lugtu <149260052+BeaLugtu@users.noreply.github.com>
2025-10-03 23:20:27 +0800

    added electron

---
+++ main.cjs
@@ -0,0 +1,31 @@
+// Electron main process
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // Load Vite dev server in development, or index.html in production
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:8080');
+  } else {
+    win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
