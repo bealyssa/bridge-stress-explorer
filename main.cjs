c2f860c059e7338ab9d6c040af963856f74b0fd4
Bea Alyssa Lugtu <149260052+BeaLugtu@users.noreply.github.com>
2025-10-05 02:03:29 +0800

    updated

--- main.cjs
+++ main.cjs
@@ -2,10 +2,34 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 1200,
-        height: 800,
+let splashWindow;
+let mainWindow;
+
+function createSplashWindow() {
+    splashWindow = new BrowserWindow({
+        width: 600,
+        height: 400,
+        frame: false,
+        alwaysOnTop: true,
+        transparent: true,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+    });
+
+    splashWindow.loadFile(path.join(__dirname, 'splash.html'));
+
+    splashWindow.on('closed', () => {
+        splashWindow = null;
+    });
+}
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1600,
+        height: 950,
+        show: false, // Don't show immediately
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
@@ -14,10 +38,29 @@ function createWindow() {
 
     // Load Vite dev server in development, or index.html in production
     if (process.env.NODE_ENV === 'development') {
-        win.loadURL('http://localhost:8080');
+        mainWindow.loadURL('http://localhost:8080');
     } else {
-        win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+        mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'));
     }
+
+    // Once main window is ready, hide splash and show main
+    mainWindow.once('ready-to-show', () => {
+        setTimeout(() => {
+            if (splashWindow) {
+                splashWindow.close();
+            }
+            mainWindow.show();
+        }, 3000); // Show splash for 3 seconds minimum
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+function createWindow() {
+    createSplashWindow();
+    createMainWindow();
 }
 
 app.whenReady().then(createWindow);
